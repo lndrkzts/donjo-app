3af77a54dbe45263915e9819721d5a5ccd64029c
Leandro <leandrocastosa@gmail.com>
2020-10-13 17:55:05 -0300

    Agrego barra de busqueda + Pantalla de busqueda + Refactor pantalla ofertas

--- apps/productos/urls.py
+++ apps/productos/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'productos'
 
 urlpatterns = [
+    path('busqueda', views.ProductoBusquedaListView.as_view(), name='busqueda'),
     path('ofertas', views.ProductosEnOfertaListView.as_view(), name='ofertas'),
     path('<slug:slug>', views.ProductoDetailView.as_view(), name='producto'),
 ]
--- apps/productos/views.py
+++ apps/productos/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
@@ -7,10 +8,15 @@ from apps.categorias.models import Categoria
 
 
 class ProductosEnOfertaListView(ListView):
-    template_name = 'productos/listado_productos.html'
+    template_name = 'productos/ofertas.html'
     context_object_name = 'lista_productos'
     queryset = Producto.objects.filter(oferta=True)
     paginate_by = 12
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cantidad_productos'] = context['lista_productos'].count()
+        return context
 
 
 class ProductoDetailView(DetailView):
@@ -22,3 +28,21 @@ class ProductoDetailView(DetailView):
 
     def get_queryset(self):
         return Producto.objects.filter(slug=self.get_slug())
+
+
+class ProductoBusquedaListView(ListView):
+    template_name = 'productos/busqueda.html'
+    context_object_name = 'lista_productos'
+
+    def get_queryset(self):
+        filtro = Q(titulo__icontains=self.query()) | Q(categoria__nombre__icontains=self.query())
+        return Producto.objects.filter(filtro)
+
+    def query(self):
+        return self.request.GET.get('q')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.query()
+        context['cantidad_productos'] = context['lista_productos'].count()
+        return context
