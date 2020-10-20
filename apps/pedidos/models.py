73412d59c2b76b54a2b3851a99d452f141fa78f5
Leandro <leandrocastosa@gmail.com>
2020-10-20 19:35:17 -0300

    Modelo de producto + archivo requirements.txt

--- apps/pedidos/models.py
+++ apps/pedidos/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from apps.usuarios.models import Usuario
+from apps.carritos.models import Carrito
+
+
+class Estado(models.TextChoices):
+    ELIMINADO = 'Eliminado'
+    CREADO = 'Creado'
+    PAGO = 'Pago'
+    EN_PREPARACION = 'En Preparacion'
+    PREPARADO = 'Preparado'
+    ENVIADO = 'Enviado'
+    ENTREGADO = 'Entregado'
+
+
+class Pedido(models.Model):
+    id_pedido = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    carrito = models.ForeignKey(Carrito, on_delete=models.CASCADE)
+    estado = models.CharField(max_length=50, choices=Estado)
+    costo_envio = models.DecimalField(max_digits=12, decimal_places=2)
+    total = models.DecimalField(max_digits=12, decimal_places=2)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.id_pedido
