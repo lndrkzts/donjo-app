2b0479c12ea106fffec9341c538b41598f6c1f62
Leandro <leandrocastosa@gmail.com>
2020-10-25 19:58:50 -0300

    Agrego cloudinary

--- donjo/settings/prod.py
+++ donjo/settings/prod.py
@@ -4,6 +4,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+INSTALLED_APPS += [
+    'cloudinary_storage',
+    'cloudinary',
+]
+
 MIDDLEWARE += [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
@@ -20,3 +25,11 @@ DATABASES = {
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+CLOUDINARY_STORAGE  = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
