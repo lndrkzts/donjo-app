25c1b05872506e37ec20dc50b8c53cbb32af5702
Leandro <leandrocastosa@gmail.com>
2020-10-28 03:50:23 -0300

    Modifico url pedidos

--- apps/pedidos/urls.py
+++ apps/pedidos/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'pedidos'
 
 urlpatterns = [
+    path('', views.PedidosListView.as_view(), name='pedidos'),
     path('pedido', views.pedido, name='pedido'),
-    path('pedidos', views.PedidosListView.as_view(), name='pedidos'),
 ]
